ed1f645ef616843cb40cdfd383f96ce2b207d843
Joshmyhu <jcasey3@my.harrisburgu.edu>
2024-09-29 05:57:31 -0400

    updated version of the game
    as title says updated version of the game with a tile map in the files for the actual building of the world.

---
+++ Drpg game copy version 2 ( newest)/Drpg game/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+      
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
