52e6f3fd8ee812e963bc8b07f74b8a1954d2a527
KingpinKamii <allisonpoole17@gmail.com>
2024-09-29 16:59:32 -0400

    Animation and Prefabs
    Added the animations for the Kid in the hub world. Also turned the trees and player in the hub world into prefabs.

--- Drpg game - Copy/Assets/Scripts/PlayerMovement.cs
+++ Drpg game - Copy/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     public float moveSpeed;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     private Vector2 moveDirection;
 
@@ -27,6 +28,10 @@ public class NewBehaviourScript : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
 
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
